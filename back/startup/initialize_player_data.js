211fb8861c9392526cb45686ac7915b893c48aa5
DrTinkle <roykoljonen@hotmail.com>
2024-11-27 03:10:02 +0200

    Persistent player data testing

--- back/startup/initialize_player_data.js
+++ back/startup/initialize_player_data.js
@@ -1,6 +1,7 @@
 const initializeBank = require('./bank_initializer');
 const initializeTeams = require('./team_randomizer');
 const initializeSchedule = require('./scheduler');
+const { loadJsonData } = require('../utils/json_helpers');
 
 function initializePlayerData(req) {
   const playerID = req?.playerID;
@@ -8,6 +9,12 @@ function initializePlayerData(req) {
   if (!playerID) {
     console.error('PlayerID not found in req:', req);
     throw new Error('PlayerID is required to initialize player data.');
+  }
+
+  const existingTeamsData = loadJsonData('teams.json', req);
+  if (existingTeamsData.length > 0) {
+    console.log(`Player data already exists for playerID: ${playerID}. Skipping initialization.`);
+    return;
   }
 
   console.log(`Initializing player data for playerID: ${playerID}...`);
